1c0c86f3f333dfd86befbb47d245c8a6c2e0a30d
Israel Alencar de França <alenc4r25@gmail.com>
2023-12-28 09:54:11 -0400

    feat:criando arquivo docker-compose com dois containers

--- src/test/java/br/com/alura/screenmatch/ScreenmatchApplicationTests.java
+++ src/test/java/br/com/alura/screenmatch/ScreenmatchApplicationTests.java
@@ -3,11 +3,11 @@ package br.com.alura.screenmatch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ScreenmatchApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class ScreenmatchApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
